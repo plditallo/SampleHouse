61a0b1f84252dad4886e8e23cb37a07d9ca207ee
Braden Bell <fish0859@gmail.com>
2020-12-30 09:06:26 -0700

    getUser moved to restricted (DRY), currency route setup

--- server/api/middleware/restricted.js
+++ server/api/middleware/restricted.js
@@ -1,6 +1,9 @@
 const {
   verify
 } = require("jsonwebtoken")
+const {
+  getUserById
+} = require("../../../database/model/userModel")
 
 module.exports = (req, res, next) => {
   const {
@@ -10,12 +13,20 @@ module.exports = (req, res, next) => {
   if (!authorization) return res.status(400).json({
     msg: "No authorization token provided"
   })
-
+  //todo check to be sure token is expiring
   verify(authorization, process.env.JWT_SECRET, (err, decodedToken) => {
     if (err) return res.status(401).json({
       msg: "Invalid token"
     })
-    req.decodedToken = decodedToken;
-    next();
+    // req.decodedToken = decodedToken;
+    const user_id = decodedToken.subject
+
+    getUserById(user_id).then(([user]) => {
+      if (!user) return res.status(403).json({
+        msg: 'The user ID: ' + user_id + ' is not associated with any account.'
+      })
+      req.user = user
+      next();
+    })
   });
 };
--- server/api/routes/purchaseRouter.js
+++ server/api/routes/purchaseRouter.js
@@ -4,7 +4,6 @@ const {
     validateOffer
 } = require("../middleware/purchaseMiddleware");
 const {
-    getUserById,
     updateUser
 } = require("../../../database/model/userModel")
 const {
@@ -15,46 +14,60 @@ const {
 const {
     insertInvoice
 } = require("../../../database/model/invoiceModel");
-const day = 86400000
-// const day = 500
+// const day = 86400000
+const day = 1000
 //todo purchase history, no duplicate downloads
 router.post("/subscribe", validatePlan, (req, res) => {
-    const user_id = req.decodedToken.subject;
-    const plan = req.plan
+    const {
+        user,
+        plan
+    } = req;
 
-    getUserById(user_id).then(([user]) => {
-        if (!user) return res.status(403).json({
-            msg: 'The user ID: ' + user_id + ' is not associated with any account.'
-        });
-        getSubscriberById(user_id).then(([subscriber]) => {
-            const subscriptionData = {
-                user_id: user.id,
-                plan_id: plan.id,
-                subscribe_start: Date.now(),
-                subscribe_end: Date.now() + (day * plan.day_length)
-            }
-            // console.log((subscriber && (subscriber.subscribe_end - Date.now()) > day / 2))
-            if (subscriber && (subscriber.subscribe_end - Date.now()) > day)
-                return res.status(200).send({
-                    msg: "User already has an active subscription. Subscription expires on: " + new Date(subscriber.subscribe_end).toLocaleDateString() + "."
-                })
-            else if (subscriber) removeSubscription(subscriber.id).then(null)
-
-            if (paymentResponse = true) return insertSubscription(subscriptionData).then(() => {
-                createInvoice(user, plan)
-                res.status(200).send({
-                    msg: "Subscription to: " + plan.name + " was successful."
-                })
+    getSubscriberById(user.id).then(([subscriber]) => {
+        const subscriptionData = {
+            user_id: user.id,
+            plan_id: plan.id,
+            subscribe_start: Date.now(),
+            subscribe_end: Date.now() + (day * plan.day_length)
+        }
+        // console.log((subscriber && (subscriber.subscribe_end - Date.now()) > day / 2))
+        //* check if subscriber has MORE than 24 hours left on subscription
+        if (subscriber && (subscriber.subscribe_end - Date.now()) > day)
+            return res.status(200).send({
+                msg: "User already has an active subscription. Subscription expires on: " + new Date(subscriber.subscribe_end).toLocaleDateString() + "."
             })
-            console.log("payment failed at new subscriber")
+        else if (subscriber) removeSubscription(subscriber.id).then(null)
+
+        if (paymentResponse = true) return insertSubscription(subscriptionData).then(() => {
+            createInvoice(user, plan)
+            res.status(200).send({
+                msg: "Subscription to: " + plan.name + " was successful."
+            })
         })
+        console.log("payment failed at new subscriber")
     })
 });
 
 router.post("/currency", validateOffer, (req, res) => {
-    res.status(200).send({
-        msg: "currency route",
-        offer: req.offer
+    const {
+        user,
+        offer
+    } = req;
+    getSubscriberById(user.id).then(([subscriber]) => {
+        //* check if subscriber has LESS than 24 hours left on subscription
+        if (!subscriber || (subscriber && (subscriber.subscribe_end - Date.now()) < day)) {
+            if (subscriber) removeSubscription(subscriber.id).then(null)
+            return res.status(200).send({
+                msg: "User doesn't have an active subscription."
+            })
+        }
+        if (paymentResponse = true) {
+            createInvoice(user, offer)
+            res.status(200).send({
+                msg: "Purchase of: " + offer.name + " was successful. " + offer.credits + "tokens have been added to your account. Your new balance is: " + user.balance + " ."
+            })
+        }
+        console.log("payment failed at new subscriber")
     })
 })
 
