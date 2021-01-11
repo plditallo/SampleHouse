8a56ef447c1398d7096fa93b0fe8dbe8ddf244ca
Braden Bell <fish0859@gmail.com>
2021-01-11 12:08:39 -0700

    cra client added and updated package.json

---
+++ client/src/utils/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, authenticated, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={() => (authenticated ? <Component /> : <Redirect to="login" />)}
+    />
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    authenticated: state.userReducer.authenticated,
+  };
+};
+export default connect(mapStateToProps, {})(PrivateRoute);
