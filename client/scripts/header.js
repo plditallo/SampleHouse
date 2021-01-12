2d2ac0dfdfaa3330497c36ec11f0ca75cd8bd739
Braden Bell <fish0859@gmail.com>
2021-01-11 18:03:07 -0700

    Sitemap w/ home page started

---
+++ client/scripts/header.js
@@ -0,0 +1,67 @@
+"use strict";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    if (this.state.liked) {
+      return "You liked this.";
+    }
+    return (
+      <div>
+        <div>
+          <img
+            src="../assets/logo.png"
+            alt="sample.House Logo"
+            style={{ width: "2em", height: "2em" }}
+          />
+          <h5>Sample House</h5>
+        </div>
+
+        <div>
+          <a>
+            {/* //todo change to icon? */}
+            <img
+              src="../assets/logo.png"
+              style={{ width: "2em", height: "2em" }}
+            />{" "}
+            support@sample.house
+          </a>
+          <div>
+            <a href="#login.html">Login</a>
+            <a href="#register.html">Sign Up</a>
+          </div>
+        </div>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#Home">Home</a>
+            </li>
+            <li>
+              <a href="#About">About</a>
+            </li>
+            <li>
+              <a href="#Products">Products</a>
+            </li>
+            <li>
+              <a href="#Pricing">Pricing</a>
+            </li>
+            <li>
+              <a href="#FAQ">FAQ's</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+const domContainer = document.querySelector("#header");
+ReactDOM.render(React.createElement(Header), domContainer);
---
+++ client/scripts/product.js
@@ -0,0 +1,51 @@
+"use strict";
+
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const data = [
+      {
+        img: "../assets/logo.png",
+        alt: "img alt",
+        title: "Title1",
+        text:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut cumque,temporibus iure facere asperiores illo,",
+      },
+      {
+        img: "../assets/logo.png",
+        alt: "img alt",
+        title: "Title2",
+        text:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut cumque,temporibus iure facere asperiores illo,",
+      },
+      {
+        img: "../assets/logo.png",
+        alt: "img alt",
+        title: "Title3",
+        text:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut cumque,temporibus iure facere asperiores illo,",
+      },
+      {
+        img: "../assets/logo.png",
+        alt: "img alt",
+        title: "Title4",
+        text:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut cumque,temporibus iure facere asperiores illo,",
+      },
+    ];
+    return data.map((e) => (
+      <div class="product">
+        <img src={e.img} alt={e.alt} />
+        <h5>{e.title}</h5>
+        <p>{e.text}</p>
+      </div>
+    ));
+  }
+}
+
+const domContainer = document.querySelector("#products");
+ReactDOM.render(React.createElement(Product), domContainer);
