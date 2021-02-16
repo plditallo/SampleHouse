36aa74245f9f54a046567c8d33c86e47b1d476f7
Braden Bell <fish0859@gmail.com>
2021-02-16 10:52:36 -0700

    edited user test seed

--- database/seeds/users.js
+++ database/seeds/users.js
@@ -8,36 +8,40 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('User').insert([{
-        id: "f0889260-4acf-11eb-91bb-4b49c1af32be",
-        email: 'jack@testing.com',
-        password: hashSync('password', 13),
-        isVerified: true,
-        first_name: "Jack",
-        last_name: "Barry",
-        role: "admin",
-        balance: 20,
-        created: Date.now()
-      }, {
-        id: "f0889260-4acf-11eb-91bb-4b4493019fcc",
-        email: 'test@test.com',
-        password: hashSync('password', 13),
-        isVerified: true,
-        first_name: "Test",
-        last_name: "user",
-        role: "testing",
-        balance: 20,
-        created: Date.now()
-      }, {
-        id: "de4fc280-59d4-11eb-aff7-07250d070e64",
-        email: "craigfechtermusic@gmail.com",
-        password: hashSync('password', 13),
-        isVerified: true,
-        first_name: "Craig",
-        last_name: "Fechter",
-        role: "admin",
-        balance: 1000000,
-        vst_access: true,
-        created: Date.now()
-      }]);
+          id: "f0889260-4acf-11eb-91bb-4b49c1af32be",
+          email: 'jack@testing.com',
+          password: hashSync('password', 13),
+          isVerified: true,
+          first_name: "Jack",
+          last_name: "Barry",
+          role: "admin",
+          balance: 20,
+          created: Date.now()
+        },
+        {
+          id: "f0889260-4acf-11eb-91bb-4b4493019fcc",
+          email: 'test@test.com',
+          password: hashSync('password', 13),
+          isVerified: true,
+          first_name: "Test",
+          last_name: "user",
+          role: "test",
+          active_subscription: true,
+          vst_access: true,
+          created: Date.now()
+        },
+        {
+          id: "de4fc280-59d4-11eb-aff7-07250d070e64",
+          email: "craigfechtermusic@gmail.com",
+          password: hashSync('password', 13),
+          isVerified: true,
+          first_name: "Craig",
+          last_name: "Fechter",
+          role: "admin",
+          balance: 1000000,
+          vst_access: true,
+          created: Date.now()
+        }
+      ]);
     });
 };
